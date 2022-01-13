530e4ddf6c5dd2383471ea1522035d8b5f9f1fed
qwertyn58 <86965418+qwertyn58@users.noreply.github.com>
2022-01-13 17:02:07 +0100

    Update p7_flask.py

--- p7_flask.py
+++ p7_flask.py
@@ -10,23 +10,25 @@ seuil = pickle.load(open("seuil.pkl","rb"))
 df_clients = pd.read_csv('df_clients.csv')
 
 #defining a route for only post requests
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET'])
 def predict():
 
     #getting an array of features from the post request's body
     sk_id = request.args
-    feature_array = df_clients[df_clients['SK_ID_CURR']==int(request.args['sk_id'])].iloc[:,1:].values
-    
-    #creating a response object
-    #storing the model's prediction in the object
-    response = {}
-    if model.predict_proba(feature_array)[0,1]>seuil:
-        response['predictions'] = 1
-    else:
-        response['predictions'] = 0   
-
-    #returning the response object as json
-    return jsonify(response,model.predict_proba(feature_array)[0,1])
+    if sk_id in df_clients['SK_ID_CURR'].values:
+        feature_array = df_clients[df_clients['SK_ID_CURR']==int(request.args['sk_id'])].iloc[:,1:].values
+        
+        #creating a response object
+        #storing the model's prediction in the object
+        response = {}
+        if model.predict_proba(feature_array)[0,1]>seuil:
+            response['predictions'] = 1
+        else:
+            response['predictions'] = 0   
+        #returning the response object as json
+        return jsonify(response,model.predict_proba(feature_array)[0,1])
+    else :
+        return jsonify('identifiant incorrect')
     
 
 if __name__ == "__main__":
